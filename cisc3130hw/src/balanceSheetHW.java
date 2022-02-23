dfb6357c8f720d7a83242cdcf54ca5834d7e979d
nvhiii <nahi.khan83@bcmail.cuny.edu>
2022-02-23 14:24:17 -0500

    working on hw1 for lowenthal

---
+++ cisc3130hw/src/balanceSheetHW.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class balanceSheetHW {
+
+    public static void main(String args) {
+
+        File file = new File(/*include file path here */);
+        Scanner stdin = new Scanner(file);
+
+        // include 2 file inputs
+        // This will include the master file and a transaction file
+        // include conditionals regarding the characters "O" and "P" adhering to the assignment
+
+        // refer back to 3115 notes in order to do good coding !
+    }
+}
